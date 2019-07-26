6a4529a7d680781bb924b4a22051bc76980d0ad5
Damien Montoya <daminor26@gmail.com>
2019-07-26 13:21:52 +0200

    done

---
+++ public/assets/js/contact.js
@@ -0,0 +1,19 @@
+let contactLoaded = false;
+let contactDisplayOffset = 0;
+
+(function(){
+    contactDisplayOffset = contactAnchor.offsetTop;
+
+    contactScroll();
+    window.addEventListener("scroll", function(){
+        contactScroll();
+    });
+})();
+
+function contactScroll(){
+    contactDisplayOffset = contactAnchor.offsetTop;
+    if(window.pageYOffset >= (contactDisplayOffset-(window.innerHeight/2)) && contactLoaded === false) {
+        contactLoaded = true;
+        displayOneAfterOther(document.querySelectorAll(".contact-item"), "opacity 0.5s linear forwards", 0.5, true, 0.3);
+    }
+}

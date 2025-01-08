a9ea9fe42f8eaf655073d8d2ef43cc8cb6b1591d
MONTOYA Damien <damien.montoya26@gmail.com>
2025-01-08 15:26:31 +0100

    chore(): tempo updates

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -5,7 +5,7 @@ const contactScroll = () => {
     contactDisplayOffset = contactAnchor.offsetTop;
     if (window.scrollY >= (contactDisplayOffset - (window.innerHeight / 2)) && contactLoaded === false) {
         contactLoaded = true;
-        displayOneAfterOther(document.querySelectorAll('.contact-item'), 'opacity 0.5s linear forwards', 0.5, true, 0.3);
+        displayOneAfterOther(document.querySelectorAll('.contact-item'), 'opacity 0.5s linear forwards', 0.2, true, 0.3);
     }
 }
 
